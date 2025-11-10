78bee113622889ebee04d61ef876037f5ccb35e0
Ostap Shp <stark171705@gmail.com>
2025-11-10 18:17:51 +0200

    completed task

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,21 +1,48 @@
+import pytest
+
 from app.split_integer import split_integer
 
 
-def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+@pytest.mark.parametrize(
+   "value, parts, expected_sum",
+   [
+       (8, 1, 8),
+       (6, 2, 6),
+       (32, 6, 32)
+   ]
+)
+def test_sum_of_the_parts_should_be_equal_to_value(
+        value: int,
+        parts: int,
+        expected_sum: int
+) -> None:
+    result = split_integer(value, parts)
+
+    assert  len(result) == parts
+    assert sum(result) == expected_sum
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+    assert split_integer(16, 2) == [8, 8]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+    assert split_integer(16, 1) == [16]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+    result = split_integer(17, 4)
+
+    assert len(result) == 4
+    assert result == sorted(result)
+    assert max(result) - min(result) <= 1
+    assert max(result) != min(result)
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+    result = split_integer(4, 6)
+
+    assert len(result) == 6
+    assert sum(result) == 4
+    assert result == sorted(result)
+    assert result.count(0) == 2 and result.count(1) == 4
