1d8215402dd9793e58aaa0c94011374dc49e9bb3
Ostap Shp <stark171705@gmail.com>
2025-11-11 15:22:45 +0200

    completed task | simplified code

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -31,18 +31,8 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    result = split_integer(17, 4)
-
-    assert len(result) == 4
-    assert result == sorted(result)
-    assert max(result) - min(result) <= 1
-    assert max(result) != min(result)
+    assert split_integer(17, 4) == [4, 4, 4, 5]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    result = split_integer(4, 6)
-
-    assert len(result) == 6
-    assert sum(result) == 4
-    assert result == sorted(result)
-    assert result.count(0) == 2 and result.count(1) == 4
+    assert split_integer(4, 6) == [0, 0, 1, 1, 1, 1]
